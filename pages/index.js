07e91c4a4017ee11ba7baf7dd4935e2f9229e423
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-14 21:44:49 +0200

    Head setup

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import Head from 'next/head';
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta name='keywords' content='web development, programming'></meta>
+      </Head>
+
       <h1>Welcome to Next</h1>
     </div>
   );
