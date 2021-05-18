7b035d7d49b6b507b36b42d8cef88e84577797e2
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-18 14:14:58 +0200

    getStaticProps setup

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import Head from 'next/head';
 
 export default function Home({ articles }) {
-  console.log(articles);
   return (
     <div>
       <Head>
         <title>WebDev Newz</title>
         <meta name='keywords' content='web development, programming'></meta>
       </Head>
-
-      <h1>Welcome to Next</h1>
     </div>
   );
 }
