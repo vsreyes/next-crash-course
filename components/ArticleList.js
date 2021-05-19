dd55397599eb01d89249afab28a111783e8a78ee
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-19 13:12:17 +0200

    Showing data setup

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,10 +1,11 @@
+import ArticleItem from './ArticleItem';
 import articleStyles from '../styles/Article.module.css';
 
 const ArticleList = ({ articles }) => {
   return (
     <div className={articleStyles.grid}>
       {articles.map(article => (
-        <h3>{article.title}</h3>
+        <ArticleItem article={article} />
       ))}
     </div>
   );
