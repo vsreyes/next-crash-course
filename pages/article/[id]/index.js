16432dea43b576e36836f810a773e444c305714b
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-21 15:46:48 +0200

    getStaticPaths setup

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,16 +1,32 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-const article = () => {
+const article = ({ article }) => {
   // const router = useRouter()
   // const {id} = router.query
 
-  return <div>This is an article {id}</div>;
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Go Back</Link>
+    </>
+  );
 };
 
-export const getServerSideProps = async context => {
+export const getStaticProps = async context => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
   );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
 };
 
 export default article;
