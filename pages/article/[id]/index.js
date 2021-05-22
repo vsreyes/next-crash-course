dc62b83936ec0dbed3ef1e5b91b3d00b6f4d5ca6
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-22 18:05:28 +0200

    getStaticPath setup

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -29,4 +29,18 @@ export const getStaticProps = async context => {
   };
 };
 
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  const articles = await res.json();
+
+  const ids = articles.map(article => article.id);
+  const paths = ids.map(id => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 export default article;
