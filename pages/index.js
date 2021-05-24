8a3df1bcaf535b63bdd99987264d5a075c793675
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-24 15:16:22 +0200

    Adding API data setup

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { server } from '../config';
 import Head from 'next/head';
 import ArticleList from '../components/ArticleList';
 
@@ -15,6 +16,17 @@ export default function Home({ articles }) {
 }
 
 export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`);
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
+
+/*export const getStaticProps = async () => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=6`
   );
@@ -25,4 +37,4 @@ export const getStaticProps = async () => {
       articles,
     },
   };
-};
+};*/
