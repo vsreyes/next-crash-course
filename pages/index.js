d1c957876e25b9322648891941dc447df5ccdc69
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-24 15:35:14 +0200

    Custom meta component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
 import { server } from '../config';
-import Head from 'next/head';
 import ArticleList from '../components/ArticleList';
 
 export default function Home({ articles }) {
   return (
     <div>
-      <Head>
-        <title>WebDev Newz</title>
-        <meta name='keywords' content='web development, programming'></meta>
-      </Head>
-
       <ArticleList articles={articles} />
     </div>
   );
